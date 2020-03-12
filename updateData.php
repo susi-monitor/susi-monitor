7384cf8dd25a8fdc66620f547a1ab753576e28c5
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-03-12 01:56:51 +0100

    Get targets list

--- updateData.php
+++ updateData.php
@@ -4,24 +4,33 @@ require_once('settings.php');
 
 function getListOfTargets($dbh)
 {
+    $stmt = $dbh->prepare('SELECT * FROM targets');
+    $stmt->execute();
+
+    return $stmt->setFetchMode(PDO::FETCH_ASSOC);
 }
 
 function checkTargets($dbh, $listOfTargets)
 {
 }
 
-$dbh = new PDO(
-    'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.'',
-    DB_USER,
-    DB_PASSWORD
-);
+try {
+    $dbh = new PDO(
+        'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME.'',
+        DB_USER,
+        DB_PASSWORD
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $error) {
+    echo "Connection failed: ".$error->getMessage();
+}
 
 $targets = getListOfTargets($dbh);
 $result = checkTargets($dbh, $targets);
 $pdo = null;
 if ($result) {
     echo 'OK';
-}else{
+} else {
     echo 'ERROR';
 }
 exit();
