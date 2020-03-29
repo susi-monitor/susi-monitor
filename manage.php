59f03a8c333e4038fcf2844d4e003397b8831303
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-03-29 15:16:04 +0200

    Create header and footer templates

--- manage.php
+++ manage.php
@@ -32,58 +32,9 @@ function getTargetData($targetId)
 }
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="SuSi Monitor">
-    <meta name="author" content="Grzegorz Olszewski <grzegorz@olszewski.in>">
-    <title><?= PAGE_TITLE ?></title>
-
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          rel="stylesheet">
-
-    <!-- Favicons TO DO-->
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
-    <!-- Custom styles -->
-    <link href="susi.css" rel="stylesheet">
-</head>
-<body>
-<header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="index.php#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarCollapse" aria-controls="navbarCollapse"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php#">Uptime status</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+<?php
+require_once ('templates/header.php');
+?>
 
 <main role="main">
     <div style="min-height: 100px;"></div>
@@ -171,18 +122,9 @@ function getTargetData($targetId)
 
     </div><!-- /.container -->
 
-
-    <!-- FOOTER -->
-    <footer class="container">
-        <p class="float-right"><a href="index.php#">Back to top</a></p>
-        <p><small><a href="https://github.com/greg-olszewski/susi-monitor" target="_blank">SuSi Monitor
-                    v<?php echo RELEASE_VERSION; ?> </a>&nbsp; All times are in <?php echo date('T') ?> .</small></p>
-    </footer>
-</main>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-<script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <?php
+    require_once ('templates/header.php');
+    ?>
 
 </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="SuSi Monitor">
+    <meta name="author" content="Grzegorz Olszewski <grzegorz@olszewski.in>">
+    <title><?= PAGE_TITLE ?></title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          rel="stylesheet">
+
+    <!-- Favicons TO DO-->
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles -->
+    <link href="susi.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarCollapse" aria-controls="navbarCollapse"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php#">Uptime status</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
