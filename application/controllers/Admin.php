0c73788141f67438a55b737d95eef39b8214af57
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-04-09 22:55:30 +0200

    Add and delete views

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,5 +1,7 @@
 <?php
-class Admin extends CI_Controller {
+
+class Admin extends CI_Controller
+{
 
     public function __construct()
     {
@@ -19,6 +21,42 @@ class Admin extends CI_Controller {
 
         $this->load->view('templates/header', $data);
         $this->load->view('admin/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function add()
+    {
+        $data['title'] = PAGE_TITLE.' - Administration';
+        $data['categories'] = $this->target_model->get_categories();
+        $data['removeFooterLinks'] = true;
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('admin/add', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function edit($targetId = NULL)
+    {
+        $data['target'] = $this->target_model->get_target_by_id($targetId)[0];
+        $data['title'] = PAGE_TITLE.' - Administration';
+        $data['target_data'] = $this->data_model->get_data();
+        $data['categories'] = $this->target_model->get_categories();
+        $data['removeFooterLinks'] = true;
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('admin/edit', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function delete($targetId = NULL)
+    {
+        $data['target'] = $this->target_model->get_target_by_id($targetId)[0];
+        $data['title'] = PAGE_TITLE.' - Administration';
+        $data['target_data'] = $this->data_model->get_data();
+        $data['removeFooterLinks'] = true;
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('admin/delete', $data);
         $this->load->view('templates/footer');
     }
 }
