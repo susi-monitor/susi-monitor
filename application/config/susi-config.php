ead73bed3c99ecf61b10a6bae752358308cdc41f
Grzegorz Olszewski <grzegorz@olszewski.in>
2020-04-29 01:54:57 +0200

    Descriptions in config file

--- application/config/susi-config.php
+++ application/config/susi-config.php
@@ -11,29 +11,50 @@ require_once ('susi-version.php');
 | -------------------------------------------------------------------
 */
 
-// Base URL of your site - typically http://example.com/ - provide with trailing slash
-// This value HAS TO be set if you wish to use a domain name
+/*
+| -------------------------------------------------------------------
+| Customization
+| -------------------------------------------------------------------
+*/
+
+/* Base URL of your site - typically http://example.com/ - provide with trailing slash
+This value HAS TO be set if you wish to use a domain name */
 defined('BASEURL')  OR define('BASEURL', '');
 
-// Custom User Agent string
-defined('UA_STRING')  OR define('UA_STRING', 'SuSi Monitor v'.RELEASE_VERSION);
-
-// MITM protection - disabled by default - set "2" to enforce checks
-defined('VERIFYHOST')  OR define('VERIFYHOST', 0);
-defined('VERIFYPEER')  OR define('VERIFYPEER', 0);
-
-//Administration panel access password
+/* Administration panel access password */
 defined('ADMIN_PASSWORD')  OR define('ADMIN_PASSWORD', 'admin');
 
-// Customized title
+/* Customized title */
 define('PAGE_TITLE', 'SuSi Monitor');
 
-// Notification settings
+/*
+| -------------------------------------------------------------------
+| Notifications
+| -------------------------------------------------------------------
+*/
 
-// Proxy settings
+/* Will not show option to enable notifications for target if set to false */
+defined('NOTIFICATIONS_ENABLED')  OR define('NOTIFICATIONS_ENABLED', false);
+
+/* Webhook URL for Microsoft Teams
+  Instruction on how to obtain that: https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook */
+defined('TEAMS_WEBHOOK_URL')  OR define('TEAMS_WEBHOOK_URL', '');
+
+/*
+| -------------------------------------------------------------------
+| Connection and client parameters
+| -------------------------------------------------------------------
+*/
+
+/* Custom User Agent string */
+defined('UA_STRING')  OR define('UA_STRING', 'SuSi Monitor v'.RELEASE_VERSION);
+
+/* Proxy settings */
 defined('PROXY_ENABLED')  OR define('PROXY_ENABLED', 0);
 defined('PROXY_HOST')  OR define('PROXY_HOST', 'exampleproxy.local');
 defined('PROXY_PORT')  OR define('PROXY_PORT', 8080);
-defined('PROXY_CREDENTIALS')  OR define('PROXY_CREDENTIALS', "someuser:somepassword");
+defined('PROXY_CREDENTIALS')  OR define('PROXY_CREDENTIALS', 'someuser:somepassword');
 
-
+/* MITM protection - disabled by default - set "2" to enforce checks */
+defined('VERIFYHOST')  OR define('VERIFYHOST', 0);
+defined('VERIFYPEER')  OR define('VERIFYPEER', 0);
